8e28c9cff8ac4f4661b9730755ccb4730464c49d
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2012-10-17 20:56:57 -0400

    Adds the executable

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-var net = require('net')
-  , es = require('event-stream')
 
-var connection = net.connect('23', 'rainmaker.wunderground.com', function () {
-  connection.write('\n' + process.argv[1] + '\n')
-
-  var c = 0
-  var f = es.mapSync(function (line) {
-    if (c++ === 1) { return line }
-  })
-
-  connection.pipe(f).pipe(process.stdout)
-
-  connection.on('end', function () {
-    console.log()
-    process.exit(0)
-  })
-
-  connection.write('X\n')
-})
+module.exports = require('./lib')
